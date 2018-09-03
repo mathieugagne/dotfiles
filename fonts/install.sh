1700834eff2aad6cce90d3819492c1f8d4d7ebdf
Mathieu Gagne <gagne.mathieu@hotmail.com>
2018-09-03 18:03:05 -0400

    Adds monofur & hack fonts while updating font awesome

--- fonts/install.sh
+++ fonts/install.sh
@@ -1,4 +1,13 @@
 #!/usr/bin/env sh
 
+# Install fontawesome
 sudo mkdir -p /usr/share/fonts/opentype/fontawesome
-sudo cp ~/dotfiles/fonts/fontawesome-free-5.2.0-desktop/otfs/* /usr/share/fonts/opentype/fontawesome/
+sudo cp ~/dotfiles/fonts/fontawesome-free-5.3.1-desktop/otfs/* /usr/share/fonts/opentype/fontawesome/
+
+# Install monofur
+sudo mkdir -p /usr/share/fonts/truetype/monofur
+sudo cp ~/dotfiles/fonts/monofur/*.ttf /usr/share/fonts/truetype/monofur/
+
+# Install hack
+sudo mkdir -p /usr/share/fonts/truetype/hack
+sudo cp ~/dotfiles/fonts/hack/*.ttf /usr/share/fonts/truetype/hack/
