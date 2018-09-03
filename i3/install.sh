69d86c35ccef941b76ed1698f8cf9de482668ec1
Mathieu Gagne <gagne.mathieu@hotmail.com>
2018-09-03 18:26:50 -0400

    Install rofi

--- i3/install.sh
+++ i3/install.sh
@@ -5,7 +5,7 @@ SHA256:baa43dbbd7232ea2b5444cae238d53bebb9d34601cc000e82f11111b1889078a
 sudo dpkg -i ./keyring.deb
 echo "deb http://debian.sur5r.net/i3/ $(grep '^DISTRIB_CODENAME=' /etc/lsb-release | sudo cut -f2 -d=) universe" >> /etc/apt/sources.list.d/sur5r-i3.list
 sudo apt update
-sudo apt install i3 nitrogen compton feh polybar numlockx
+sudo apt install i3 nitrogen compton feh polybar numlockx rofi
 ln -fs ~/dotfiles/i3/config ~/.config/i3/config
 ln -fs ~/dotfiles/i3/polybar/config ~/.config/polybar/config
 
