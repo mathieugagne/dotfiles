26f9d473abb8766936f8c592a78c4b33bd412517
Mathieu Gagne <mat@potloc.com>
2018-09-04 17:57:19 -0400

    Fixes polybar install script

--- i3/polybar/install.sh
+++ i3/polybar/install.sh
@@ -12,10 +12,16 @@ libiw-dev \
 libnl-3-dev \
 wireless-tools \
 libasound2-dev \
-libcurl4-openssl-dev
+libcurl4-openssl-dev \
+libmpdclient-dev
 
 git clone --branch 3.2 --recursive https://github.com/jaagr/polybar ~/.local/lib/polybar
-cd ~/.local/lib/polybar/ && ./build.sh -f
+cd ~/.local/lib/polybar/
+rm -rf build
+mkdir -p build
+cd build
+cmake ..
+sudo make install
 
 mkdir -p ~/.config/polybar
 ln -fs ~/dotfiles/i3/polybar/config ~/.config/polybar/config
