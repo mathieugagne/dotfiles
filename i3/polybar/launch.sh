a5f62ebf3dd84094ae6964b138ebe20ebf5c0c1f
Mathieu Gagne <mat@potloc.com>
2018-09-04 17:19:57 -0400

    Load environment variables

--- i3/polybar/launch.sh
+++ i3/polybar/launch.sh
@@ -1,10 +1,12 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 # Terminate already running bar instances
 killall -q polybar
 
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+source ~/dotfiles/i3/polybar/.env
 
 # Launch top and bottom bars
 polybar top &
