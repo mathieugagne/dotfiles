947c73e5ec2080239a429f88cdb471edbfac83d7
Mathieu Gagne <gagne.mathieu@hotmail.com>
2018-09-03 21:15:07 -0400

    Adds noto, material, unifont

--- fonts/install.sh
+++ fonts/install.sh
@@ -4,6 +4,9 @@ cp ~/dotfiles/fonts/fontawesome-free-5.3.1-desktop/otfs/*.otf ~/.local/share/fon
 cp ~/dotfiles/fonts/fontawesome/*.ttf ~/.local/share/fonts/
 cp ~/dotfiles/fonts/monofur/*.ttf ~/.local/share/fonts/
 cp ~/dotfiles/fonts/hack/*.ttf ~/.local/share/fonts/
+cp ~/dotfiles/fonts/noto/*.ttf ~/.local/share/fonts/
+cp ~/dotfiles/fonts/unifont/*.ttf ~/.local/share/fonts/
+cp ~/dotfiles/fonts/material/*.ttf ~/.local/share/fonts/
 
 # Build font information cache files
 fc-cache -fv
