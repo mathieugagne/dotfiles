5511da6ec3d97e4e926293c86ca9525073266d34
Mathieu Gagne <gagne.mathieu@hotmail.com>
2018-09-05 08:26:56 -0400

    Moves scripts to bin folder

--- i3/polybar/bin/potloc-email.sh
+++ i3/polybar/bin/potloc-email.sh
@@ -2,7 +2,7 @@
 
 source ~/dotfiles/i3/polybar/.env
 
-inbox=$(curl -sf -u "$GMAIL_USER":"$GMAIL_PASS" -X "STATUS INBOX (UNSEEN)" imaps://imap.gmail.com/INBOX | tr -d -c "[:digit:]")
+inbox=$(curl -sf -u "$POTLOC_USER":"$POTLOC_PASS" -X "STATUS INBOX (UNSEEN)" imaps://imap.gmail.com/INBOX | tr -d -c "[:digit:]")
 
 if [ "$inbox" ] && [ "$inbox" -gt 0 ]; then
     echo "$inbox"
