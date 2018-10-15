f6185cab3fe49c4db005728a52a34db46da6bdd8
Mathieu Gagne <gagne.mathieu@hotmail.com>
2018-10-15 16:41:25 -0400

    Adds vscode install

--- install.sh
+++ install.sh
@@ -12,6 +12,7 @@ docker/install.sh
 gotop/install.sh
 sublime-text/install.sh
 spotify/install.sh
+vscode/install.sh
 
 # Tweaks
 echo fs.inotify.max_user_watches=524288 | sudo tee -a /etc/sysctl.conf && sudo sysctl -p
