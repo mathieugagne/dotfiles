0e9993ec268d89d3513caebb4c7b62a1ee1c8942
Mathieu Gagné <gagne.mathieu@hotmail.com>
2019-12-16 09:50:32 -0500

    Adds VPN status

---
+++ i3/polybar/bin/nordvpn.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+nordvpn_status=$(nordvpn status | grep Status | awk '{ print $2 }')
+
+if [ $nordvpn_status = "Connected" ]; then
+  echo "%{F#55aa55} %{F-}"
+else
+  echo "%{F#ff5555}%{F-}"
+fi
