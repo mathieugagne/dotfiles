817221110aceed20ba9a033c5dc2af6088deec0b
Mathieu Gagné <gagne.mathieu@hotmail.com>
2019-12-17 11:14:48 -0500

    Updates i3wm

--- i3/install.sh
+++ i3/install.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env sh
 
-/usr/lib/apt/apt-helper download-file http://debian.sur5r.net/i3/pool/main/s/sur5r-keyring/sur5r-keyring_2018.01.30_all.deb keyring.deb
-SHA256:baa43dbbd7232ea2b5444cae238d53bebb9d34601cc000e82f11111b1889078a
-sudo dpkg -i ./keyring.deb
-echo "deb http://debian.sur5r.net/i3/ $(grep '^DISTRIB_CODENAME=' /etc/lsb-release | sudo cut -f2 -d=) universe" >> /etc/apt/sources.list.d/sur5r-i3.list
-sudo apt update
-sudo apt install i3 nitrogen compton feh numlockx rofi scrot
+sudo su -
+/usr/lib/apt/apt-helper download-file https://debian.sur5r.net/i3/pool/main/s/sur5r-keyring/sur5r-keyring_2019.02.01_all.deb keyring.deb SHA256:176af52de1a976f103f9809920d80d02411ac5e763f695327de9fa6aff23f416
+dpkg -i ./keyring.deb
+echo "deb https://debian.sur5r.net/i3/ $(grep '^DISTRIB_CODENAME=' /etc/lsb-release | cut -f2 -d=) universe" >> /etc/apt/sources.list.d/sur5r-i3.list
+apt update
+apt install i3 nitrogen compton feh numlockx rofi scrot
+rm keyring.deb
+logout
 
 mkdir -p ~/.config/i3
 ln -fs ~/dotfiles/i3/config ~/.config/i3/config
