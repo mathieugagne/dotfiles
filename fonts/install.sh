354510aad2f0987274bb1eaeb50010077c11ec4c
Mathieu Gagné <gagne.mathieu@hotmail.com>
2019-12-21 13:56:57 -0500

    Adds UbuntuMono font

--- fonts/install.sh
+++ fonts/install.sh
@@ -10,5 +10,13 @@ cp ~/dotfiles/fonts/unifont/*.ttf ~/.local/share/fonts/
 cp ~/dotfiles/fonts/material/*.ttf ~/.local/share/fonts/
 cp ~/dotfiles/fonts/weather/*.ttf ~/.local/share/fonts/
 
+sudo mkdir -p /opt/nerd-fonts/
+sudo chown $USER: /opt/nerd-fonts
+
+cd /opt/nerd-fonts
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.0.0/UbuntuMono.zip
+unzip UbuntuMono.zip
+cp *.ttf ~/.local/share/fonts/
+
 # Build font information cache files
 fc-cache -fv
