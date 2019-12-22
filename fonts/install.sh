7f4168bacf12b09879757edf72f2eafaee000c80
Mathieu Gagné <gagne.mathieu@hotmail.com>
2019-12-21 21:26:37 -0500

    Get fonts from nerd-fonts

--- fonts/install.sh
+++ fonts/install.sh
@@ -1,11 +1,6 @@
 #!/usr/bin/env sh
 
 mkdir -p ~/.local/share/fonts/
-cp ~/dotfiles/fonts/fontawesome-free-5.3.1-desktop/otfs/*.otf ~/.local/share/fonts/
-cp ~/dotfiles/fonts/fontawesome/*.ttf ~/.local/share/fonts/
-cp ~/dotfiles/fonts/monofur/*.ttf ~/.local/share/fonts/
-cp ~/dotfiles/fonts/hack/*.ttf ~/.local/share/fonts/
-cp ~/dotfiles/fonts/noto/*.ttf ~/.local/share/fonts/
 cp ~/dotfiles/fonts/unifont/*.ttf ~/.local/share/fonts/
 cp ~/dotfiles/fonts/material/*.ttf ~/.local/share/fonts/
 cp ~/dotfiles/fonts/weather/*.ttf ~/.local/share/fonts/
@@ -14,9 +9,20 @@ sudo mkdir -p /opt/nerd-fonts/
 sudo chown $USER: /opt/nerd-fonts
 
 cd /opt/nerd-fonts
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.0.0/UbuntuMono.zip
-unzip UbuntuMono.zip
-cp *.ttf ~/.local/share/fonts/
+
+wget -nc https://github.com/ryanoasis/nerd-fonts/releases/download/v2.0.0/UbuntuMono.zip
+wget -nc https://github.com/ryanoasis/nerd-fonts/releases/download/v2.0.0/Hack.zip
+wget -nc https://github.com/ryanoasis/nerd-fonts/releases/download/v2.0.0/LiberationMono.zip
+wget -nc https://github.com/ryanoasis/nerd-fonts/releases/download/v2.0.0/Noto.zip
+wget -nc https://github.com/ryanoasis/nerd-fonts/releases/download/v2.0.0/Monofur.zip
+
+unzip -f UbuntuMono.zip
+unzip -f Hack.zip
+unzip -f LiberationMono.zip
+unzip -f Noto.zip
+unzip -f Monofur.zip
+
+mv *.ttf ~/.local/share/fonts/
 
 # Build font information cache files
 fc-cache -fv
