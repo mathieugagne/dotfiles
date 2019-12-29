1cd09f60a2572698e1b889c480cceb0c22b94822
Mathieu Gagné <gagne.mathieu@hotmail.com>
2019-12-29 17:36:07 -0500

    Better vpn icon

--- i3/polybar/bin/nordvpn.sh
+++ i3/polybar/bin/nordvpn.sh
@@ -9,7 +9,7 @@ fi
 nordvpn_status=$(nordvpn status | grep Status | awk '{ print $2 }')
 
 if [ $nordvpn_status = "Connected" ]; then
-  echo "%{F#55aa55} %{F-}"
+  echo "%{F#55aa55}%{F-}"
 else
   echo "%{F#ff5555}%{F-}"
 fi
