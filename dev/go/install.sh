149bb7888e9ffebad03d973edc904f822076cbd8
Mathieu Gagné <gagne.mathieu@hotmail.com>
2020-01-07 09:48:34 -0500

    Auto yes apt commands

--- dev/go/install.sh
+++ dev/go/install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 
-sudo add-apt-repository ppa:longsleep/golang-backports
+sudo add-apt-repository -y ppa:longsleep/golang-backports
 sudo apt-get update -y
 sudo apt-get install -y golang-go
