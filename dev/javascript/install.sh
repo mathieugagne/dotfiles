6e2ae0bc71c2616deb9477aaf8045e358cdb862f
Mathieu Gagné <gagne.mathieu@hotmail.com>
2020-02-01 23:00:35 -0500

    Install n globally

--- dev/javascript/install.sh
+++ dev/javascript/install.sh
@@ -5,7 +5,7 @@ curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 sudo apt install -y nodejs
 
 # n
-npm install -g n
+sudo npm install -g n
 
 # optionally
-# sudo apt remove nodejs
+sudo apt remove nodejs
