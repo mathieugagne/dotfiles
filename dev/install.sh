704b6daa6d7c9441e7463eca593ac0e5f63c80be
Mathieu Gagné <gagne.mathieu@hotmail.com>
2020-02-01 23:34:07 -0500

    Extra lines

--- dev/install.sh
+++ dev/install.sh
@@ -5,9 +5,7 @@ dev/go/install.sh
 dev/python/install.sh
 dev/javascript/install.sh
 
-
-
 # Jira CLI
 go get github.com/go-jira/jira/cmd/jira
 rm -rf ~/.jira.d/
-ln -fs ~/dotfiles/dev/.jira.d ~/.jira.d
+ln -fs ~/dotfiles/dev/.jira.d/ ~/.jira.d
