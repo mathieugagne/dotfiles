82fd36cd1d63f3bc38038ddea0fbeab31af6d4e3
Mathieu Gagné <gagne.mathieu@hotmail.com>
2020-02-01 22:47:23 -0500

    Fixes spotify/install.sh

--- spotify/install.sh
+++ spotify/install.sh
@@ -1,15 +1,6 @@
 #!/usr/bin/env sh
 
-# Instructions from: https://www.spotify.com/ca-en/download/linux/
-
-# 1. Add the Spotify repository signing keys to be able to verify downloaded packages
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 931FF8E79F0876134EDDBDCCA87FF9DF48BF1C90
-
-# 2. Add the Spotify repository
-echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
-
-# 3. Update list of available packages
+curl -sS https://download.spotify.com/debian/pubkey.gpg | sudo apt-key add - 
+echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
 sudo apt update
-
-# 4. Install Spotify
 sudo apt install -y spotify-client
