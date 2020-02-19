d43bfc5d796ca7997619ca1afbdffb14d66073c3
Mathieu Gagné <gagne.mathieu@hotmail.com>
2020-02-19 07:10:16 -0500

    Adds jira as binstub

--- dev/install.sh
+++ dev/install.sh
@@ -9,3 +9,10 @@ dev/javascript/install.sh
 go get github.com/go-jira/jira/cmd/jira
 rm -rf ~/.jira.d/
 ln -fs ~/dotfiles/dev/.jira.d/ ~/.jira.d
+
+# Github CLI
+wget https://github.com/github/hub/releases/download/v2.14.1/hub-linux-amd64-2.14.1.tgz
+tar -xzvf hub-linux-amd64-2.14.1.tgz
+rm hub-linux-amd64-2.14.1.tgz
+sudo ./hub-linux-amd64-2.14.1/install
+rm -rf hub-linux-amd64-2.14.1
