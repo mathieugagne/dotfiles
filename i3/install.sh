6af276ee835fba6c0378e53fbe9d972136a913c7
Mathieu Gagné <gagne.mathieu@hotmail.com>
2020-03-28 18:30:09 -0400

    Updates i3

--- i3/install.sh
+++ i3/install.sh
@@ -5,9 +5,7 @@ sudo apt-get update
 sudo apt-get install -y i3-gaps rofi nitrogen numlockx
 
 mkdir -p ~/.config/i3
-ln -fs ~/dotfiles/i3/config ~/.config/i3/config
+ln -fs ~/dotfiles/i3/config.ini ~/.config/i3/config
 
 # https://faq.i3wm.org/question/1/how-can-i-get-rid-of-the-nautilus-desktop-window.1.html
 gsettings set org.gnome.desktop.background show-desktop-icons false
-
-~/dotfiles/i3/polybar/install.sh
