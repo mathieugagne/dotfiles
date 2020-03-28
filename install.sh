1ffc8cdd4ef5fdf6617be65729db5d6430ead195
Mathieu Gagné <gagne.mathieu@hotmail.com>
2020-03-28 18:30:18 -0400

    Updates global install script

--- install.sh
+++ install.sh
@@ -4,9 +4,11 @@ utils/install.sh
 shell/install.sh
 fonts/install.sh
 vim/install.sh
-i3/install.sh
 dev/install.sh
 docker/install.sh
+i3/install.sh
+polybar/install.sh
+dunst/install.sh
 # sublime-text/install.sh
 spotify/install.sh
 vscode/install.sh
