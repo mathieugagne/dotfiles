724ffd75b1f4dbdb1682548fb9e17c98cfc9b6dc
Mathieu Gagné <gagne.mathieu@hotmail.com>
2020-03-29 12:21:12 -0400

    Moves shell dotfiles to .config

---
+++ .config/install.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+
+ln -fs ~/dotfiles/.config/.xinitrc ~/.xinitrc
+ln -fs ~/dotfiles/.config/.bashrc ~/.bashrc
+ln -fs ~/dotfiles/.config/.bash_aliases ~/.bash_aliases
+ln -fs ~/dotfiles/.config/.profile ~/.profile
+ln -fs ~/dotfiles/.config/.gitconfig ~/.gitconfig
+ln -fs ~/dotfiles/.config/.zshrc ~/.zshrc
--- shell/urxvt/install.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-
-sudo apt update
-sudo apt install -y rxvt-unicode
-
-ln -fs ~/dotfiles/.Xresources ~/.Xresources
-xrdb ~/.Xresources
