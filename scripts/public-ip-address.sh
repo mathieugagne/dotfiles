4c8760b9bcb3b1148b5d4422c7014736e567018f
Mathieu Gagné <gagne.mathieu@hotmail.com>
2020-03-29 14:02:27 -0400

    Renames ippublic

--- scripts/public-ip-address.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-ip=$(curl -s 'https://api.ipify.org?format=json' | jq -r ".ip")
-vpn_status=$(~/dotfiles/scripts/nordvpn.sh)
-
-# TODO: If $vpn_status is nil then display globe icon
-
-echo $vpn_status $ip
