a87fce22f0af7d932d30672ad5441a479f04d0af
Mathieu Gagné <gagne.mathieu@hotmail.com>
2020-04-06 18:00:58 -0400

    Adds font-size urxvt extension

--- urxvt/install.sh
+++ urxvt/install.sh
@@ -2,3 +2,7 @@
 
 sudo apt update
 sudo apt install -y rxvt-unicode
+
+# Install extension to increase/decrease font-size
+mkdir -p ~/.urxvt/ext
+git clone https://github.com/majutsushi/urxvt-font-size ~/.urxvt/ext/font-size
