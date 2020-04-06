19fa478153f3dac882bf7f8d4c7f33d8e14f7d03
Mathieu Gagné <gagne.mathieu@hotmail.com>
2020-04-06 17:58:06 -0400

    Also adds .xsession to home folder

--- .config/install.sh
+++ .config/install.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 
 ln -fs ~/dotfiles/.config/.xinitrc ~/.xinitrc
+ln -fs ~/dotfiles/.config/.xinitrc ~/.xsession
 ln -fs ~/dotfiles/.config/.bashrc ~/.bashrc
 ln -fs ~/dotfiles/.config/.bash_aliases ~/.bash_aliases
 ln -fs ~/dotfiles/.config/.profile ~/.profile
