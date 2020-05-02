0308df66731aef055f643e79f464d5cc85aa5c00
Mathieu Gagné <gagne.mathieu@hotmail.com>
2020-05-02 12:16:54 -0400

    Updates docker install script

--- docker/install.sh
+++ docker/install.sh
@@ -1,9 +1,19 @@
 #!/usr/bin/env sh
 
+sudo apt-get purge -y docker-ce docker-ce-cli containerd.io
+
+sudo apt update -y
+sudo apt install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt update
-sudo apt install -y docker-ce
+sudo apt update -y
+sudo apt install -y docker-ce docker-ce-cli containerd.io
 
 # Run docker as non-root
 sudo groupadd docker
