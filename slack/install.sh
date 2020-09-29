399b7795e2ea8e96afd2a5d3df07c43e015e922c
Mathieu Gagné <gagne.mathieu@hotmail.com>
2020-09-29 13:59:58 -0400

    Fixes slack install

--- slack/install.sh
+++ slack/install.sh
@@ -2,9 +2,6 @@
 
 sudo apt purge -y slack
 
-wget https://downloads.slack-edge.com/linux_releases/slack-desktop-4.0.2-amd64.deb
-sudo dpkg -i slack-desktop-4.0.2-amd64.deb
+wget https://downloads.slack-edge.com/linux_releases/slack-desktop-4.9.1-amd64.deb
+sudo dpkg -i slack-desktop-4.9.1-amd64.deb
 rm slack-desktop-*.deb
-
-sudo apt update
-sudo apt install -y slack
