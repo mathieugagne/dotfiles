fd87b8e7536bec0ba5bed77f05702d7da8174405
Mathieu Gagné <gagne.mathieu@hotmail.com>
2020-09-29 09:26:01 -0400

    Removes jira cli config

--- dev/install.sh
+++ dev/install.sh
@@ -6,11 +6,6 @@ dev/go/install.sh
 dev/python/install.sh
 dev/javascript/install.sh
 
-# Jira CLI
-go get github.com/go-jira/jira/cmd/jira
-rm -rf ~/.jira.d/
-ln -fs ~/dotfiles/dev/.jira.d/ ~/.jira.d
-
 # Github CLI
 wget https://github.com/github/hub/releases/download/v2.14.1/hub-linux-amd64-2.14.1.tgz
 tar -xzvf hub-linux-amd64-2.14.1.tgz
