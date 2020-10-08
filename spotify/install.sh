1cf3c5a7133951fdbd2e90272b82b8d214849b64
Mathieu Gagné <gagne.mathieu@hotmail.com>
2020-10-08 08:59:16 -0400

    Updates spotify client install

--- spotify/install.sh
+++ spotify/install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-curl -sS https://download.spotify.com/debian/pubkey.gpg | sudo apt-key add - 
+curl -sS https://download.spotify.com/debian/pubkey_0D811D58.gpg | sudo apt-key add -
 echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
 sudo apt update
 sudo apt install -y spotify-client
