8fba3c5b8a642e9714a68c250193eb0051d00424
Mathieu Gagné <gagne.mathieu@hotmail.com>
2020-12-03 17:01:36 -0500

    Updates docker-compose

--- docker/install.sh
+++ docker/install.sh
@@ -21,5 +21,5 @@ sudo usermod -aG docker $(whoami)
 
 # docker-compose
 # https://docs.docker.com/compose/install/#install-compose
-sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+sudo curl -L https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
