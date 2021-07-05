6112eb30dc780dea6e974cc54fd15366002c1765
Mathieu Gagné <gagne.mathieu@hotmail.com>
2021-07-05 17:22:43 -0400

    Fixes docker-compose install

--- docker/install.sh
+++ docker/install.sh
@@ -21,5 +21,8 @@ sudo usermod -aG docker $(whoami)
 
 # docker-compose
 # https://docs.docker.com/compose/install/#install-compose
-sudo curl -L https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+sudo curl -L https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+
+# Ensure it isn't already installed with python as a pip package
+pip uninstall docker-compose
