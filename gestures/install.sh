5e7ca6d1b9695dffdb833b6864e550242b80f81f
Mathieu Gagné <gagne.mathieu@hotmail.com>
2021-07-05 10:38:39 -0400

    Sets natural scrolling on touchpad

--- gestures/install.sh
+++ gestures/install.sh
@@ -6,3 +6,7 @@ git clone https://github.com/bulletmark/libinput-gestures.git /tmp/libinput-gest
 cd /tmp/libinput-gestures
 sudo make install
 
+# Follow instructions here to invert vertical scrolling:
+# https://askubuntu.com/questions/1122513/how-to-add-natural-inverted-mouse-scrolling-in-i3-window-manager
+# TODO: Use `xinput set-prop` to automatically set this
+
