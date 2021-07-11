7d6aa32c76fd0c3909893922fc5093d46399d4ba
Mathieu Gagné <gagne.mathieu@hotmail.com>
2021-07-11 12:31:03 -0400

    Configures gestures for Mac-like behavior

--- gestures/install.sh
+++ gestures/install.sh
@@ -5,8 +5,12 @@ sudo apt install wmctrl xdotool libinput-tools
 git clone https://github.com/bulletmark/libinput-gestures.git /tmp/libinput-gestures
 cd /tmp/libinput-gestures
 sudo make install
+sudo gpasswd -a $USER input
 
 # Follow instructions here to invert vertical scrolling:
 # https://askubuntu.com/questions/1122513/how-to-add-natural-inverted-mouse-scrolling-in-i3-window-manager
 # TODO: Use `xinput set-prop` to automatically set this
 
+ln -fs ~/dotfiles/gestures/libinput-gestures.conf ~/.config/libinput-gestures.conf
+
+libinput-gestures-setup autostart start
