ced8213540f3cbb794cb30163b244b6a778311f5
Mathieu Gagné <gagne.mathieu@hotmail.com>
2021-07-11 11:35:09 -0400

    Updates Slack

--- slack/install.sh
+++ slack/install.sh
@@ -2,6 +2,6 @@
 
 sudo apt purge -y slack
 
-wget https://downloads.slack-edge.com/linux_releases/slack-desktop-4.9.1-amd64.deb
-sudo dpkg -i slack-desktop-4.9.1-amd64.deb
+wget https://downloads.slack-edge.com/linux_releases/slack-desktop-4.17.0-amd64.deb
+sudo dpkg -i slack-desktop-4.17.0-amd64.deb
 rm slack-desktop-*.deb
