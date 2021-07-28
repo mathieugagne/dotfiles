bc6e6fa401fdc6b57d7ce0fc26aa803add3dadc0
Mathieu Gagné <gagne.mathieu@hotmail.com>
2021-07-27 23:18:41 -0400

    Adds backlight module

---
+++ backlight/install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cat << EOF | sudo tee /etc/udev/rules.d/backlight.rules
+RUN+="/bin/chgrp video /sys/class/backlight/intel_backlight/brightness"
+RUN+="/bin/chmod g+w /sys/class/backlight/intel_backlight/brightness"
+EOF
