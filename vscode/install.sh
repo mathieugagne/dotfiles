d424779340db4b0fd046da4a7fb39b0b2cd41cf5
Mathieu Gagné <gagne.mathieu@hotmail.com>
2021-12-08 11:01:41 -0500

    Adds notes for VSCode sync

--- vscode/install.sh
+++ vscode/install.sh
@@ -6,5 +6,10 @@ sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
 sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
 
 sudo apt install apt-transport-https
-sudo apt update
+sudo apt update -y
 sudo apt install code # or code-insiders
+
+
+# Setup Online Settings Services with your Github account
+#  to sync settings from your other installations
+# This will automatically install shortcuts, extensions, themes, etc.
