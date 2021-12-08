6ee66aecc1c059d2ff1120ec50b54d101b203840
Mathieu Gagné <gagne.mathieu@hotmail.com>
2021-12-08 11:50:40 -0500

    Updates Slack install

--- slack/install.sh
+++ slack/install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
-sudo apt purge -y slack
+# Visit https://slack.com/downloads/linux and download the .deb file
+# The following doesn't work anymore
+# wget https://downloads.slack-edge.com/linux_releases/slack-desktop-4.22.0-amd64.deb
 
-wget https://downloads.slack-edge.com/linux_releases/slack-desktop-4.17.0-amd64.deb
-sudo dpkg -i slack-desktop-4.17.0-amd64.deb
-rm slack-desktop-*.deb
+sudo dpkg -i ~/dotfiles/slack/slack-desktop-4.22.0-amd64.deb
