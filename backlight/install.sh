6f71ea7879c166ca247357947c89fb5139ba10ac
Mathieu Gagné <gagne.mathieu@hotmail.com>
2021-12-12 19:01:58 -0500

    Fixes polybar backlight permissions

--- backlight/install.sh
+++ backlight/install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+sudo chmod a+rw /sys/class/backlight/intel_backlight/brightness
+
 cat << EOF | sudo tee /etc/udev/rules.d/backlight.rules
 RUN+="/bin/chgrp video /sys/class/backlight/intel_backlight/brightness"
 RUN+="/bin/chmod g+w /sys/class/backlight/intel_backlight/brightness"
