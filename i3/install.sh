1d231f5253f2636ce910091ad58f15ac9043f1b6
Mathieu Gagné <gagne.mathieu@hotmail.com>
2022-01-12 10:57:12 -0500

    Adds xcwd to keep track of current directory when launching new terminals

--- i3/install.sh
+++ i3/install.sh
@@ -10,3 +10,13 @@ ln -fs ~/.config/dotfiles/.i3.env ~/dotfiles/i3/.env
 
 # https://faq.i3wm.org/question/1/how-can-i-get-rid-of-the-nautilus-desktop-window.1.html
 gsettings set org.gnome.desktop.background show-desktop-icons false
+
+# Install small c program to keep track of current directory
+# This allows us to open a new terminal on the same path as we last were
+# https://github.com/schischi/xcwd
+wget https://github.com/schischi/xcwd/archive/master.zip -O /tmp/xcwd.zip
+unzip /tmp/xcwd.zip -d /tmp
+cd /tmp/xcwd-master
+sudo make install
+rm -rf /tmp/xcwd.zip /tmp/xcwd-master
+cd ~/dotfiles
