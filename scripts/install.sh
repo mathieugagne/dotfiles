07076df643900f83f8c30aad6b8009457865386d
Mathieu Gagné <gagne.mathieu@hotmail.com>
2022-02-14 12:10:57 -0500

    Adds required lib for screenshot

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env sh
 
+# For the `import` lib used in `screenshot`
+sudo apt install imagemagick-6.q16
+
 mkdir -p ~/.config/dotfiles
 touch ~/dotfiles/scripts/.env
 ln -fs ~/.config/dotfiles/.scripts.env ~/dotfiles/scripts/.env
