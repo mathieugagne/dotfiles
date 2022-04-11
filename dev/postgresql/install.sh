fb87fd898569353004808cf9e9c17ce2b7ced16c
Mathieu Gagné <gagne.mathieu@hotmail.com>
2022-04-11 17:38:36 -0400

    Adds postgresql-client to dev installation

---
+++ dev/postgresql/install.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env sh
+
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+echo "deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`-pgdg main" |sudo tee  /etc/apt/sources.list.d/pgdg.list
+
+sudo apt update -y
+sudo apt install -y postgresql-client-13
