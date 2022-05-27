d6f50370c8db0765b5b325b7b15582b0af83acf8
Mathieu Gagné <gagne.mathieu@hotmail.com>
2022-05-27 10:54:56 +0200

    Splits i3 config in laptop vs desktop

--- i3/install.sh
+++ i3/install.sh
@@ -4,10 +4,6 @@ sudo add-apt-repository -y ppa:kgilmer/speed-ricer
 sudo apt-get update
 sudo apt-get install -y i3-gaps numlockx
 
-mkdir -p ~/.config/i3
-ln -fs ~/dotfiles/i3/config.ini ~/.config/i3/config
-ln -fs ~/.config/dotfiles/.i3.env ~/dotfiles/i3/.env
-
 # https://faq.i3wm.org/question/1/how-can-i-get-rid-of-the-nautilus-desktop-window.1.html
 gsettings set org.gnome.desktop.background show-desktop-icons false
 
@@ -20,3 +16,9 @@ cd /tmp/xcwd-master
 sudo make install
 rm -rf /tmp/xcwd.zip /tmp/xcwd-master
 cd ~/dotfiles
+
+# Select which computer you are on and apply the right configuration
+mkdir -p ~/.config/i3
+ln -fs ~/dotfiles/i3/config.laptop.ini ~/.config/i3/config
+# ln -fs ~/dotfiles/i3/config.desktop.ini ~/.config/i3/config
+ln -fs ~/.config/dotfiles/.i3.env ~/dotfiles/i3/.env
