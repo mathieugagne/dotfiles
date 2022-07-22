17381183829e12741518b6d2a1986a32e9eaad88
Mathieu Gagné <gagne.mathieu@hotmail.com>
2022-07-22 08:42:00 -0400

    Updates postgresql install

--- dev/postgresql/install.sh
+++ dev/postgresql/install.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env sh
 
+# https://wiki.postgresql.org/wiki/Apt
+
+sudo apt install curl ca-certificates gnupg
+curl https://www.postgresql.org/media/keys/ACCC4CF8.asc | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/apt.postgresql.org.gpg >/dev/null
+
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
 echo "deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`-pgdg main" |sudo tee  /etc/apt/sources.list.d/pgdg.list
 
 sudo apt update -y
-sudo apt install -y postgresql-client-13
+sudo apt install -y postgresql-client-14
