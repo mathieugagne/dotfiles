a694a91b0bee4e8edda0d83fa6675045eff6bb97
Mathieu Gagné <gagne.mathieu@hotmail.com>
2022-09-09 11:53:11 -0400

    Installs xbacklight and redshift

--- laptop/backlight/install.sh
+++ laptop/backlight/install.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+# TODO: Create Xorg.conf.d/2-intel.conf config file
+# https://wiki.archlinux.org/title/backlight#xbacklight
+
+sudo apt install xbacklight redshift
+
 sudo chmod a+rw /sys/class/backlight/intel_backlight/brightness
 
 cat << EOF | sudo tee /etc/udev/rules.d/backlight.rules
 RUN+="/bin/chgrp video /sys/class/backlight/intel_backlight/brightness"
 RUN+="/bin/chmod g+w /sys/class/backlight/intel_backlight/brightness"
 EOF
+
+sudo usermod -a -G video mathieu
