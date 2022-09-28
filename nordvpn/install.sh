a5c718e3fee2ddd0f7d449465195e624019e5203
Mathieu Gagné <gagne.mathieu@hotmail.com>
2022-09-27 23:16:34 -0400

    Adds nordvpn installer

---
+++ nordvpn/install.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env sh
+
+wget -q https://repo.nordvpn.com/deb/nordvpn/debian/pool/main/nordvpn-release_1.0.0_all.deb -O /tmp/nordvpn.deb
+sudo apt install /tmp/nordvpn.deb
+sudo apt update -y
+sudo apt install nordvpn
