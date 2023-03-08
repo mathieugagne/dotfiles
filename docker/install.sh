a3965bec486e6c6b3e7fb94fd3e709018e67514a
Mathieu Gagné <gagne.mathieu@hotmail.com>
2023-03-08 08:47:00 -0500

    Install docker compose plugin instead of standalone version

--- docker/install.sh
+++ docker/install.sh
@@ -13,16 +13,11 @@ sudo apt install -y \
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt update -y
-sudo apt install -y docker-ce docker-ce-cli containerd.io
+sudo apt install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
 
 # Run docker as non-root
 sudo groupadd docker
 sudo usermod -aG docker $(whoami)
-
-# docker-compose
-# https://docs.docker.com/compose/install/#install-compose
-sudo curl -L https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
 
 # Ensure it isn't already installed with python as a pip package
 pip uninstall docker-compose
